98ef6d530092e29a909bebf89dfb452cb04e7f64
larissa <99204156+lal4mp@users.noreply.github.com>
2024-08-26 12:14:31 -0300

    feat: add description part to recipe page

--- app/recipePage/[id]/page.tsx
+++ app/recipePage/[id]/page.tsx
@@ -18,68 +18,76 @@ const RecipePage = () => {
             {/* Main */}
             <main className="flex flex-col items-center gap-2 lg:flex-row lg:gap-3 lg:flex-1">
                 <div className="flex flex-col items-center gap-2 lg:w-4/5 xl:w-2/5 ">
-                {/* Nome */}
-                <h1 className="text-xl text-rose-950 font-bold">NOME DA RECEITA</h1>
-
-                {/* Infos: avaliação, tempo e porções */}
-                <div className="flex gap-3 w-fit px-3 justify-center rounded-lg bg-rose-950 text-white">
-                    {/* Avaliação */}
-                    <Button className="flex gap-1 font-normal pr-4">
-                        <FaRegStar className="w-4 h-4" />
-                        <p>5.0</p>
-                    </Button>
-
-                    {/* Tempo */}
-                    <div className="flex gap-1 text-sm font-normal items-center px-4">
-                        <IoIosTimer className="w-5 h-5" />
-                        <p>2h</p>
+                    {/* Nome */}
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-xl text-rose-950 font-bold">NOME DA RECEITA</h1>
+                        <h3 className="text-rose-950 text-sm">receita de @username</h3>
                     </div>
 
-                    {/* Porções */}
-                    <div className="flex gap-1 text-sm font-normal items-center px-4">
-                        <FaBowlFood className="w-5 h-4" />
-                        <p>5</p>
+                    {/* Infos: avaliação, tempo e porções */}
+                    <div className="flex gap-3 w-fit px-3 justify-center rounded-lg bg-rose-950 text-white">
+                        {/* Avaliação */}
+                        <Button className="flex gap-1 font-normal pr-4">
+                            <FaRegStar className="w-4 h-4" />
+                            <p>5.0</p>
+                        </Button>
+
+                        {/* Tempo */}
+                        <div className="flex gap-1 text-sm font-normal items-center px-4">
+                            <IoIosTimer className="w-5 h-5" />
+                            <p>2h</p>
+                        </div>
+
+                        {/* Porções */}
+                        <div className="flex gap-1 text-sm font-normal items-center px-4">
+                            <FaBowlFood className="w-5 h-4" />
+                            <p>5</p>
+                        </div>
+                    </div>
+
+                    {/* Categorias */}
+                    <div className="flex w-4/5 items-center justify-center">
+                        <div className="flex gap-1 items-center text-nowrap overflow-auto [&::-webkit-scrollbar]:hidden">
+                            <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Almoço</p>
+                            <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Jantar</p>
+                            <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Saudável</p>
+                            <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Fast-food</p>
+                            <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Doce</p>  
+                        </div>  
+                    </div>
+
+                    {/* Imagem e Vídeo */}
+                    <Carousel className="w-[400px] h-[250px]">
+                        <CarouselContent className="h-[250px] w-[400px] m-0 place-items-center">
+                            <CarouselItem className="p-0 m-0">
+                                <Image
+                                    alt="Imagem da receita"
+                                    src="/recipe-photo.jpg"
+                                    width={400}
+                                    height={250}
+                                />
+                            </CarouselItem>
+                            <CarouselItem className="p-0 m-0">
+                                <Image
+                                    alt="Imagem da receita"
+                                    src="/recipe-photo.jpg"
+                                    width={400}
+                                    height={250}
+                                />
+                            </CarouselItem>
+                        </CarouselContent>
+                        <CarouselPrevious className="bg-rose-950 text-white ml-2"/>
+                        <CarouselNext className="bg-rose-950 text-white mr-2"/>
+                    </Carousel>
+
+                    {/* Descrição */}
+                    <div className="flex w-4/5 bg-rose-950 text-white text-sm p-2 rounded-md">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum in corporis qui, voluptate expedita eum debitis ea repudiandae natus cumque architecto magnam quos tenetur doloribus eos tempore provident quasi repellat.</p>
                     </div>
                 </div>
 
-                {/* Categorias */}
-                <div className="flex w-4/5 items-center justify-center">
-                    <div className="flex gap-1 items-center text-nowrap overflow-auto [&::-webkit-scrollbar]:hidden">
-                        <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Almoço</p>
-                        <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Jantar</p>
-                        <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Saudável</p>
-                        <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Fast-food</p>
-                        <p className="text-sm bg-rose-950 text-white font-light px-2 rounded-md">Doce</p>  
-                    </div>  
-                </div>
-
-                {/* Imagem e Vídeo */}
-                <Carousel className="w-[400px] h-[250px]">
-                    <CarouselContent className="h-[250px] w-[400px] m-0 place-items-center">
-                        <CarouselItem className="p-0 m-0">
-                            <Image
-                                alt="Imagem da receita"
-                                src="/recipe-photo.jpg"
-                                width={400}
-                                height={250}
-                            />
-                        </CarouselItem>
-                        <CarouselItem className="p-0 m-0">
-                            <Image
-                                alt="Imagem da receita"
-                                src="/recipe-photo.jpg"
-                                width={400}
-                                height={250}
-                            />
-                        </CarouselItem>
-                    </CarouselContent>
-                    <CarouselPrevious className="bg-rose-950 text-white ml-2"/>
-                    <CarouselNext className="bg-rose-950 text-white mr-2"/>
-                </Carousel>
-                </div>
-
-                {/* Ingredientes, Calorias e Macros*/}
-                <div className="flex gap-6 w-4/5 lg:flex-col lg:mr-5 lg:my-3">
+                {/* Ingredientes, Calorias e Macros */}
+                <div className="flex gap-6 w-4/5 mb-5 mt-2 lg:flex-col lg:mr-5 lg:my-3">
                     {/* Ingredientes */}
                     <div className="w-full">
                         <p className="bg-rose-950 text-white font-bold px-2 rounded-md text-sm w-fit">INGREDIENTES</p>
